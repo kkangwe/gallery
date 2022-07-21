02445c28ce7bccee539cfc3c67221e07441b22a2
kkuria <kekangwe@gmail.com>
2022-07-21 22:02:23 +0300

    done

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://kkP@sscode63!>@ip.hyt3jew.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://kkP@sscode63!>@ip.hyt3jew.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://kkP@sscode63!>@ip.hyt3jew.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://kk:admin123@ip.hyt3jew.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://kk:admin123@ip.hyt3jew.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://kk:admin123@ip.hyt3jew.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
